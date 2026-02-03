fd8e5269b20cc068ec847c71e42fd449ba75bc4b
Kryštof Hrdý <hrdykrystof@gmail.com>
2026-02-03 09:17:58 +0100

    bugfix:for module version missmatch

--- src/app/(dashboard)/troop/[troopId]/meetings/page.tsx
+++ src/app/(dashboard)/troop/[troopId]/meetings/page.tsx
@@ -1,11 +1,23 @@
 "use client";
 
-import { useParams } from "next/navigation";
-import { Id } from "../../../../../../convex/_generated/dataModel";
-import { useState } from "react";
-
-import MeetingsTab from "../../../../../components/MeetingsTab";
+import RadyTab from "../../../../../components/RadyTab";
 
 export default function MeetingsPage() {
-  return <MeetingsTab />;
+  return (
+    <div style={{ width: "100%", position: "relative", overflowX: "hidden" }}>
+        {/* Top Title Bar */}
+        <div style={{
+            backgroundColor: "white",
+            borderBottom: "3px solid #000",
+            padding: "1rem 2rem",
+            margin: "0 -2rem 2rem -2rem",
+            width: "calc(100% + 4rem)",
+            display: "flex", justifyContent: "space-between", alignItems: "center"
+        }}>
+            <h1 style={{ fontSize: "1.5rem", fontWeight: "900", margin: 0 }}>Rady a Dokumentace</h1>
+        </div>
+
+        <RadyTab />
+    </div>
+  );
 }
