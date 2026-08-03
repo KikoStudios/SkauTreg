4a65abcec2bdf92246a929d9f1011f6fa3e4d599
Kryštof Hrdý <hrdykrystof@gmail.com>
2026-08-03 08:41:11 +0200

    fix: simplify ticket sharing to qr

--- src/components/trip/TransportTab.tsx
+++ src/components/trip/TransportTab.tsx
@@ -2,15 +2,11 @@
 
 import { Id } from "../../../convex/_generated/dataModel";
 import DopravaTicketPage from "./DopravaTicketPage";
-import TripTicketSharePanel from "./TripTicketSharePanel";
 
 export default function TransportTab(props: {
   tripId: Id<"trips">;
   trip: { location: string; startDate: string; endDate?: string | null };
 }) {
-  return <>
-    <TripTicketSharePanel tripId={props.tripId} defaultExpiry={props.trip.endDate || props.trip.startDate} />
-    <DopravaTicketPage {...props} />
-  </>;
+  return <DopravaTicketPage {...props} />;
 }
 
